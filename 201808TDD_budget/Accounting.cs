64d683147a600128b20e42211111584ad45e6396
hatelove <jay.hatelove@gmail.com>
2018-08-11 15:53:38 +0800

    pass test: no budgets

---
+++ 201808TDD_budget/Accounting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _201808TDD_budget
+{
+    public class Accounting
+    {
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}
--- 201808TDD_budget/UnitTest1.cs
+++ 201808TDD_budget/UnitTest1.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace _201808TDD_budget
 {
     [TestClass]
-    public class UnitTest1
+    public class AccountingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void no_budgets()
         {
+            var accounting = new Accounting();
+            var start = new DateTime(2018, 3, 1);
+            var end = new DateTime(2018, 3, 1);
+            var totalAmount = accounting.TotalAmount(start, end);
+            Assert.AreEqual(0, totalAmount);
         }
     }
 }
